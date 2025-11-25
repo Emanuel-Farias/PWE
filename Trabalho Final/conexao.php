4a992738df434c7bdb909a868fae66c4e9c01ff0
Emanuel de Oliveira Farias <emanuel.oliveira.farias@gmail.com>
2025-11-25 11:21:35 -0300

    banco de dados

--- Trabalho Final/conexao.php
+++ Trabalho Final/conexao.php
@@ -5,18 +5,17 @@ function conectar()
     $servername = 'localhost';
     $username = 'root';
     $password = 'admin';
-    $dbname = 'webti';
+    $dbname = 'banco';
 
     return new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 }
 //conectar();
 
-function cadastra_usuario($nome, $email, $login, $senha)
+function cadastra_usuario($nome, $login, $senha)
 {
     $con = conectar();
-    $stmt = $con->prepare("INSERT INTO usuarios (nome, email, login, senha) VALUES (:nome, :email, :login, :senha)");
+    $stmt = $con->prepare("INSERT INTO user (nome, login, senha) VALUES (:nome, :login, :senha)");
     $stmt->bindParam(':nome', $nome);
-    $stmt->bindParam(':email', $email);
     $stmt->bindParam(':login', $login);
     $stmt->bindParam(':senha', $senha);
     return $stmt->execute();
@@ -27,17 +26,16 @@ function cadastra_usuario($nome, $email, $login, $senha)
 function delete_usuario($id)
 {
     $con = conectar();
-    $stmt = $con->prepare("DELETE FROM usuarios WHERE id = :id");
+    $stmt = $con->prepare("DELETE FROM user WHERE id = :id");
     $stmt->bindParam(":id", $id);
     return $stmt->execute();
 }
 
-function atualiza_usuario($nome, $email, $login, $senha, $id)
+function atualiza_usuario($nome, $login, $senha, $id)
 {
     $con = conectar();
-    $stmt = $con->prepare("UPDATE usuarios SET nome =:nome, email = :email, login = :login, senha = :senha WHERE id = :id");
+    $stmt = $con->prepare("UPDATE user SET nome =:nome, login = :login, senha = :senha WHERE id = :id");
     $stmt->bindParam(':nome', $nome);
-    $stmt->bindParam(':email', $email);
     $stmt->bindParam(':login', $login);
     $stmt->bindParam(':senha', $senha);
     $stmt->bindParam(':id', var: $id);
@@ -51,7 +49,7 @@ function atualiza_usuario($nome, $email, $login, $senha, $id)
 function get_usuario($id)
 {
     $con = conectar();
-    $stmt = $con->prepare("SELECT * FROM usuarios WHERE id = :id");
+    $stmt = $con->prepare("SELECT * FROM user WHERE id = :id");
     $stmt->bindParam(':id', var: $id);
     $stmt->execute();
     return $stmt->fetch(PDO::FETCH_ASSOC);
@@ -60,7 +58,7 @@ function get_usuario($id)
 
 function list_usuario() {
     $con = conectar();
-    $stmt = $con->prepare("SELECT * FROM usuarios");
+    $stmt = $con->prepare("SELECT * FROM user");
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
---
+++ Trabalho Final/user.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 25-Nov-2025 às 15:20
+-- Versão do servidor: 8.4.4
+-- versão do PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `banco`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `user`
+--
+
+CREATE TABLE `user` (
+  `nome` varchar(24) NOT NULL,
+  `login` varchar(24) NOT NULL,
+  `senha` varchar(24) NOT NULL,
+  `id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `user`
+--
+
+INSERT INTO `user` (`nome`, `login`, `senha`, `id`) VALUES
+('ema', 'ema', 'ema', 1),
+('ema', 'ema', 'ema', 2);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
